46d8d6865c218cdfc37d4e7e6d9be19d0d4dc2ed
Prakhar Agarwal <prakhargvp@gmail.com>
2023-01-22 07:32:50 +0530

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Lottery DAPP</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="text-6xl font-bold">
+        Lets build an awesome lottery WEB3 DAPP    
+      </h1>
+
+    </div>
+  )
+}
+
+export default Home
